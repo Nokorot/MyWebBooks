20da924b67936a29de3e7658b9bcce178da8cbcf
Nokorot <torhaakon@hotmail.com>
2023-01-24 00:55:22 +0100

    Tue Jan 24 00:55:22 CET 2023

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, jsonify, send_file
+import os, sys
+
+sys.path.insert(0, './html_to_epub')
+
+from html_to_epub.RR_rss import load_datafile
+
+from sendToKindle import main as sendToKindle
 
 app = Flask(__name__)
+
+
+@app.route('/RR_rss')
+def RR_rss():
+    # new, epub_file = load_datafile('./books/IR.yaml')
+    # if not new:
+    #     return "No updates!"
+
+    epub_file = 'out/IR-2023-01-24.epub'
+
+    sendToKindle(epub_file)
+
+    return """
+    <html> <p>A new update was sent to the kindle <p>
+    <a href="/dl-latest-epub" target="blank"><button class='btn btn-default'>Download!</button></a>
+    """
+    
+
+    # return jsonify({"new": new, "data": data})
+    # return send_file(epub_file, as_attachment=True, attachment_filename=epub_file)
+
+@app.route('/dl-latest-epub')
+def dl_latest_epub():
+    epub_file = 'out/IR-2023-01-24.epub'
+    return send_file(epub_file, as_attachment=True, attachment_filename=epub_file)
+
 
 
 @app.route('/')
---
+++ test.py
@@ -0,0 +1,6 @@
+
+import a.b.Alpha as a
+
+print(a.variable)
+
+
