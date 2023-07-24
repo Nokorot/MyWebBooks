6855c1a90b9640a9b467d3d4a2dcce9f3bf453b4
HealyTea12 <37420604+HealyTea12@users.noreply.github.com>
2023-07-24 19:03:56 +0200

    hellow world

---
+++ src/auth.py
@@ -0,0 +1,11 @@
+import functools
+
+from flask import redirect, url_for, g 
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('login'))
+        return view(**kwargs)
+    return wrapped_view
---
+++ src/mongodb_api_1.py
@@ -0,0 +1,42 @@
+
+import pymongo
+import sys
+import os
+
+from dotenv import load_dotenv
+from pprint import pprint 
+
+# connect to mongodb
+load_dotenv()
+MONGODB_URI = os.environ['MONGODB_URI']
+
+try:
+  print("connecnting to client")
+  client = pymongo.MongoClient(MONGODB_URI)
+  print("connected")
+  
+# return a friendly error if a URI error is thrown 
+except pymongo.errors.ConfigurationError:
+  print("An Invalid URI host error was received. Is your Atlas host name correct in your connection string?")
+  sys.exit(1)
+
+def insertOne(database: str, collection: str, document: dict):
+  coll = client[database][collection]
+  coll.insert_one(document)
+
+def findOne(database: str, collection:str, query: dict):
+  coll = client[database][collection]
+  return coll.find_one(query)
+
+def find(database: str, collection:str, query: dict):
+  coll = client[database][collection]
+  return coll.find(query)
+
+def updateOne(database: str, collection: str, query: dict, update: dict):
+  coll = client[database][collection]
+  coll.update_one(query, {"$set" : update })
+
+def deleteOne(database: str, collection: str, query: dict):
+  coll = client[database][collection]
+  coll.delete_one(query)
+
