3bab232f5e3eb7e7a18d2ad7bbed12b7a9625085
Nokorot <torhaakon2012@hotmail.com>
2023-09-21 20:34:59 +0200

    Fixed the download and sendToKindle

--- src/mongodb_api_1.py
+++ src/mongodb_api_1.py
@@ -31,9 +31,9 @@ def find(database: str, collection:str, query: dict):
   coll = client[database][collection]
   return coll.find(query)
 
-def updateOne(database: str, collection: str, query: dict, update: dict):
+def updateOne(database: str, collection: str, query: dict, update: dict, **kwargs):
   coll = client[database][collection]
-  coll.update_one(query, {"$set" : update })
+  coll.update_one(query, {"$set" : update }, **kwargs)
 
 def deleteOne(database: str, collection: str, query: dict):
   coll = client[database][collection]
